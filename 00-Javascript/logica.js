21b66f6563168691cd0baaaa129993d4dddb9859
Reginaldo Miguel <reginaldomig@gmail.com>
2023-03-20 01:55:41 +0000

    w

---
+++ 00-Javascript/logica.js
@@ -0,0 +1,17 @@
+numero = 100
+contador = numero
+
+
+
+while(contador != 0){
+
+    // encontre os numeros primos entre 0 e 100
+    // esse código está errado
+    if(numero % contador != 0){
+        console.log(`O numero ${contador} é Primo`)
+    }
+
+
+    contador = contador -1
+
+}
